cd5edb7a43357cb97928374170d63c592b92ae74
Aria Love <aria.love.720@gmail.com>
2020-09-17 17:19:45 -0600

    Completed Prototype 2

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+    public GameObject projectilePrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
+        checkBounds();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fireProjectile();
+        }
+
+    }
+
+    void checkBounds()
+    {
+        // keeps player in bounds.
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        } else if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+
+    void fireProjectile()
+    {
+        //Launch a projectile from the player.
+        Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+    }
+}
